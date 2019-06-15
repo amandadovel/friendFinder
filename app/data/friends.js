e0fb631ff1bce32743404b39450cd09a6f39ec98
Amanda Dovel <amandacdovel@gmail.com>
2019-06-15 15:32:51 -0600

    fix range bar bubble

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,16 @@
 module.exports = [
     {
         name: "Sapphire",
-        image: "../public/assets/images/sergiopink.jpg",
+        image: "https://images.unsplash.com/photo-1521482911495-e36799c9d53a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
         scores: [
             "2",
             "1",
             "4",
             "4",
             "3",
-            "3",
-            "3",
-            "3",
+            "1",
+            "1",
+            "1",
             "3",
             "2"
         ]
@@ -18,7 +18,7 @@ module.exports = [
 
     {
         name: "Sergio",
-        image: "../public/assets/images/candiceblue.jpg",
+        image: "https://images.unsplash.com/photo-1501947248335-9b511c0cb5c9?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         scores: [
             "2",
             "1",
@@ -35,7 +35,7 @@ module.exports = [
 
     {
         name: "Tetre",
-        image: "../public/assets/images/tetregreen.jpg",
+        image: "https://images.unsplash.com/photo-1534060256027-5c92d62cd83e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         scores: [
             "2",
             "5",
@@ -51,8 +51,8 @@ module.exports = [
     },
 
     {
-        name: "Denz",
-        image: "../public/assets/images/denielbackflip.jpg",
+        name: "Warren",
+        image: "https://images.unsplash.com/photo-1503248947681-3198a7abfcc9?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         scores: [
             "2",
             "1",
@@ -69,7 +69,7 @@ module.exports = [
 
     {
         name: "Pixel",
-        image: "../public/assets/images/cloudypixel.jpg",
+        image: "https://images.unsplash.com/photo-1504898770365-14faca6a7320?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         scores: [
             "2",
             "1",
@@ -86,7 +86,7 @@ module.exports = [
 
     {
         name: "Yogini",
-        image: "../public/assets/images/yogini.jpg",
+        image: "https://images.unsplash.com/photo-1509205206130-24819154d9e8?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         scores: [
             "2",
             "1",
